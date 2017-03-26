a7b00a762856ba28538d1cf14990f2eb890c0104
Robin Millette <robin@millette.info>
2017-03-26 16:24:18 -0400

    Cleanup.

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,16 @@ var sassPaths = [
   'bower_components/motion-ui/src'
 ]
 
-gulp.task('sass', () => {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'))
-})
+gulp.task('sass', () => gulp.src('scss/app.scss')
+  .pipe($.sass({
+    includePaths: sassPaths,
+    outputStyle: 'compressed' // if css compressed **file size**
+  })
+    .on('error', $.sass.logError))
+  .pipe($.autoprefixer({
+    browsers: ['last 2 versions', 'ie >= 9']
+  }))
+  .pipe(gulp.dest('css'))
+)
 
-gulp.task('default', ['sass'], () => {
-  gulp.watch(['scss/**/*.scss'], ['sass'])
-})
+gulp.task('default', ['sass'], () => gulp.watch(['scss/**/*.scss'], ['sass']))
