0b897017c2b26de528c46856e9727760eb6cc1b1
Robin Millette <robin@millette.info>
2017-03-27 09:44:52 -0400

    Expand selection.

--- js/app.js
+++ js/app.js
@@ -3,3 +3,19 @@
 'use strict'
 
 $(document).foundation()
+
+$('.column.medium-4').each(function () {
+  var $a = $('a', this)
+  var a = $a[0]
+  var hash = a && a.hash && a.hash.slice(1)
+  if (!hash) { return }
+  var $par = $(this)
+  $par.data('what', hash)
+  $a.click(function (ev) {
+    $par.toggleClass('full')
+    $('[data-toggler=".hide"]').each(function () {
+      var $this = $(this)
+      if (hash !== $this.data('what')) { $this.foundation('toggle') }
+    })
+  })
+})
